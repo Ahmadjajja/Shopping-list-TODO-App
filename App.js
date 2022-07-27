99ae6eaebf34569d8a063349b7c3e237de47530d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-07-27 17:00:20 +0500

    button,indicator

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, ActivityIndicator, Text, View, Button,Alert } from 'react-native'
 import React from 'react'
 import Home from "./src/screens/home/Home"
 const App = () => {
   return (
     <View style={[styles.flexContainer]}>
+      <Home />
       <Text>
-        <Home/>
+        <ActivityIndicator size="large" color="red"/>
       </Text>
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
     </View>
   )
 }
@@ -14,9 +20,9 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({
-  flexContainer:{
-    justifyContent:center,
-    alignItems:center,
-    color:"red",
+  flexContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    color: "red",
   }
 })
