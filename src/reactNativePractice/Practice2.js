3f221cc0affa81596885a72f8eeb4a2f9336541e
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-07-28 12:46:57 +0500

    TouchAbleOpacity

--- src/reactNativePractice/Practice2.js
+++ src/reactNativePractice/Practice2.js
@@ -1,14 +1,16 @@
-import { View, Text, Button, Alert, Modal } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text, Button, Alert, Modal, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
 const Practice2 = () => {
 
   // const [modalVisible, setModalVisible] = useState(false);
-  Alert.alert('Hey Ahmad!')
+  // Alert.alert('Hey Ahmad!')
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
 
   return (
     <View>
-      <Modal
+      {/* <Modal
         animationType="fade"
         transparent={true}
         visible={true}
@@ -16,22 +18,51 @@ const Practice2 = () => {
           Alert.alert("Modal has been closed.");
           // setModalVisible(!modalVisible);
         }}
-      >
-        <Button title="press me" />
-        <Button title="press me" />
+      > */}
+        {/* <Button title="press me" /> */}
+        {/* <Button title="press me" />
         <Button
           title="press me"
           onPress={() => {
             Alert.alert("Congratulations! you Pressed this me");
             // setModalVisible(!modalVisible);
-          }} 
-          />
-        <Text>
+          }}
+        /> */}
+        {/* <Text>
           Practice2
-        </Text>
-      </Modal>
+        </Text> */}
+      {/* </Modal> */}
+      <View style={styles.countContainer}>
+        <Text>Count: {count}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
     </View>
+
+
+    // </View >
   )
 }
 
 export default Practice2
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
+});
